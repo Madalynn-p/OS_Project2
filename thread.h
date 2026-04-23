99c6c88e67b73d6a212de462206616d2c8409c0c
Madalynn-p <mjpendergr42@tntech.edu>
2026-04-22 19:08:22 -0500

    Update thread.h
    Single line of code was added for this file

--- thread.h
+++ thread.h
@@ -89,6 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+     /*NEW CODE ADDED HERE ALARM CLOCK*/
+    int64_t wake_tick;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
